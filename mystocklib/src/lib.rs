446ec6c46ace73f7ba4f26e833cdc29399faaca4
Kai Sickeler <k.sickeler@gmail.com>
2017-07-22 17:47:43 +0200

    Initial version

---
+++ mystocklib/src/lib.rs
@@ -0,0 +1,17 @@
+#[deny(non_camel_case_types,
+       non_snake_case,
+       unused_import_braces,
+       trivial_numeric_casts,
+       unstable_features,
+       unused_allocation,
+       unused_imports,
+       unused_must_use,
+       unused_mut,
+       unused_qualifications,
+       while_true,
+       unsafe_code)]
+#[test]
+
+
+fn it_works() {
+}
